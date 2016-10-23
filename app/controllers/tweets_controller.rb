d885d563f6002230ae52d0e32ffe2d1f477b7e9c
tmartinelli <thiagov.martinelli@gmail.com>
2016-10-23 16:23:53 -0200

    remove .invisible span tags in order to improve dom processing

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,10 @@
 class TweetsController < ApplicationController
-  
+
   def index
     response = HTTParty.get('http://twitter.com/newrelic')
-    
+
     parsed_data = Nokogiri::HTML.parse response.body
+    parsed_data.css(".invisible").remove
     tweetNodes = parsed_data.css(".js-tweet-text")
     @nodes = tweetNodes.collect do |node|
       node.inner_html
